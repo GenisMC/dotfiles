83039aeb8c987a2f8400eaa2e37023a1b749c7de
GenisMC <moracasado@gmail.com>
2024-02-06 20:30:51 +0100

    Fix installer and add kitty bg color catppuccin style

--- installer.sh
+++ installer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-ln -s "$(dirname "$0")/nvim" ~/.config/nvim
-ln -s "$(dirname "$0")/tmux" ~/.config/tmux
-ln -s "$(dirname "$0")/kitty" ~/.config/kitty
+ln -sn "$(pwd)/kitty" "$HOME/.config/kitty"
+ln -sn "$(pwd)/tmux" "$HOME/.config/tmux"
+ln -sn "$(pwd)/nvim" "$HOME/.config/nvim"
