c0bd40550204043e01f19e042577035025adc9fa
GenisMC <moracasado@gmail.com>
2024-07-20 13:21:17 +0200

    new installer with zsh plugins

--- installer.sh
+++ installer.sh
@@ -5,9 +5,20 @@ if [ ! -d "$HOME/.config" ]; then
   mkdir "$HOME/.config"
 fi
 
+rm -rf ~/.zsh
+rm -rf ~/.config/kitty
+rm -rf ~/.config/tmux
+rm -rf ~/.config/starship.toml
+mv ~/.config/nvim ~/.config/nvim_bck
+mv ~/.zshrc ~/.zshrc_bck
+
 ln -sn "$(pwd)/starship.toml" "$HOME/.config/starship.toml"
 ln -sn "$(pwd)/.zshrc" "$HOME/.zshrc"
 ln -sn "$(pwd)/kitty" "$HOME/.config/kitty"
 ln -sn "$(pwd)/tmux" "$HOME/.config/tmux"
 ln -sn "$(pwd)/nvim" "$HOME/.config/nvim"
 ln -sn "$(pwd)/.zsh" "$HOME/.zsh"
+
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.zsh/zsh-autosuggestions
+git clone https://github.com/marlonrichert/zsh-autocomplete.git ~/.zsh/zsh-autocomplete
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ~/.zsh/fast-syntax-highlighting
